587e484c7fa4906cf405121c43d531eef0be0584
Shivendra Shrivastwa <shivendrasince2001@gmail.com>
2023-04-25 13:57:09 +0530

    Portfolio committed successfully

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(()=>{
+    $(window).scroll(()=>{
+        if(this.scrollY>20){
+            $("#navbar").addClass("sticky");
+        }else{
+            $("#navbar").removeClass("sticky");
+        }
+    });
+
+    $('.menu-btn').click(function(){
+        $('#navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // $('.menu a').click(function(){
+    //     $('.menu a').toggleClass("active");
+    // })
+
+    $('#navbar .menu a').click(function(){
+        $('#navbar .menu').removeClass("active");
+        $('.menu-btn i').removeClass("active");
+    });
+});
+
